5b80d145ef132521e26c1c49a30ae4d14774e458
Eric <eric.nobody@gmail.com>
2016-01-18 01:48:14 -0500

    Named coroutines.

--- vm.lua
+++ vm.lua
@@ -1,24 +1,119 @@
 local VM = {}
 
+local index = 1
+local running = 0
 VM.coroutines = {}
+VM.name2co = {}
+VM.co2names = {}
 
-function VM.spawn(fun)
-  local co = coroutine.create(fun)
-  table.insert(VM.coroutines,co)
-  VM.resume(co)
-  return co
+--TODO spawn_link
+VM.coroutines[1]=coroutine.running()
+
+function VM.self()
+  return 1
 end
 
-function VM.resume(co)
-  coroutine.resume(co)
+function VM.status(co)
+  if not VM.coroutines[co] then error("badarg: Coroutine "..co.." does not exist",2) end
+  return coroutine.status(VM.coroutines[co])
+end
+
+local function unregisterNames(co)
+  for _,name in pairs(VM.co2names[co]) do
+    VM.name2co[name] = nil
+  end
+  VM.co2names[co]=nil
+end
+
+local function registerName(name,co)
+  VM.name2co[name]=co
+  if not VM.co2names[co] then
+    VM.co2names[co]={}
+  end
+  table.insert(VM.co2names[co],name)
+end
+
+function VM.unregister(name)
+  if not VM.name2co[name] then
+    error("badarg: "..name.." not a registered coroutine",2)
+  end
+  local co = VM.name2co[name]
+  VM.name2co[name]=nil
+  for x,_name in pairs(VM.co2names[co]) do
+    if _name == name then
+      table.remove(VM.co2names[co],x)
+      break
+    end
+  end
+  return true
+end
+
+local function removeCo(co)
+  --for k,v in pairs(VM.coroutines) do print(k,v) end
+  if VM.co2names[co] then
+    unregisterNames(co) end
+  VM.coroutines[co]=nil
+end
+
+local function kill(co)
+  removeCo(co)
+end
+
+
+function VM.spawn(fun)
+  index = index + 1
+  VM.coroutines[index]=coroutine.create(fun)
+  VM.resume(index)
+  return index
+end
+
+--TODO change to private function?
+function VM.resume(co,...)
+  running = co
+  local thread = VM.coroutines[co]
+  coroutine.resume(thread,unpack(arg))
+  if coroutine.status(thread)=="dead" then
+    removeCo(co)
+  end
 end
 
 function VM.send(co,...)
-  coroutine.resume(co,unpack(arg))
+  if type(co) == "string" then
+    if VM.name2co[co] then co = VM.name2co[co]
+    else error("badarg: "..co.." not a registered coroutine")
+    end
+  elseif not (type(co) == "number") then error("badarg: "..co,2) end
+  if VM.coroutines[co] then
+    VM.resume(co,unpack(arg))
+  end
 end
 
 function VM.receive()
-  return coroutine.yield()
+  local co = running
+  local function terminate(event,...)
+    if event == "terminate" then
+      kill(co)
+    else
+      return event,unpack(arg)
+    end
+  end
+  return terminate(coroutine.yield())
+end
+
+function VM.register(name,co)
+  if VM.name2co[name] then
+    error("badarg: "..name.." already registered",2)
+  elseif VM.status(co) == "dead" then
+    error("badarg: Cannot register dead coroutine "..co,2)
+  elseif not VM.coroutines[co] then
+    error("badarg: Cannot find coroutine "..co)
+  else
+    registerName(name,co)
+  end
+end
+
+function VM.registered()
+  return VM.name2co
 end
 
 return VM  
--- vm_test.lua
+++ vm_test.lua
@@ -4,14 +4,14 @@ luaunit = require 'luaunit'
 
 function test_spawn()
   local co = VM.spawn(function() coroutine.yield() end)
-  luaunit.assert_equals(coroutine.status(co),"suspended")
-  luaunit.assert_true(table.getn(VM.coroutines)>0)
+  luaunit.assert_equals(VM.status(co),"suspended")
+  luaunit.assert_true(VM.coroutines[co])
 end
 
 function test_resume()
   local co = VM.spawn(function() coroutine.yield() end)
   VM.resume(co)
-  luaunit.assert_equals(coroutine.status(co),"dead")
+  luaunit.assertErrorMsgContains("does not exist",VM.status,co)
 end
 
 function test_send()
@@ -20,6 +20,10 @@ function test_send()
   VM.send(co,"a message","second msg")
   luaunit.assert_equals(msg,"a message")
   luaunit.assert_equals(msg2,"second msg")
+  --send doesn't fail except for badarg
+  luaunit.assertError(VM.send,{"badarg"},"msg")
+  VM.send(-1,"msg")
+  luaunit.assertErrorMsgContains("badarg",VM.send,"badname","msg")
 end
 
 function test_receive()
@@ -28,6 +32,33 @@ function test_receive()
   local co = VM.spawn(function() msg2 = VM.receive() end)
   VM.send(co,msg)
   luaunit.assert_equals(msg,msg2)
+end
+
+function test_terminate()
+  local co = VM.spawn(function() VM.receive() end)
+  VM.send(co,"terminate")
+  luaunit.assertError(VM.status,co)
+  --luaunit.assertError(VM.resume,co)
+end
+
+function test_register()
+  local co = VM.spawn(function() VM.receive() end)
+  VM.register("routine",co)
+  luaunit.assertEquals(VM.registered(),{["routine"]=co})
+  VM.resume(co)
+  luaunit.assertEquals(VM.registered(),{})
+  luaunit.assertErrorMsgContains("badarg",VM.register,"routine",co)
+  local deadCo = VM.spawn(function() end)
+  luaunit.assertErrorMsgContains("badarg",VM.register,"imdead",deadCo)
+end
+
+function test_registered_co()
+  local msg
+  local co = VM.spawn(function() msg = VM.receive() end)
+  local name = "name"
+  VM.register(name,co)
+  VM.send(name,"msg")
+  luaunit.assertEquals(msg,"msg")
 end
 
 os.exit(luaunit.LuaUnit.run())
